ea83be91725252e526d72ddf7f9cf807457f7fbd
INI_LABIL <115417258+Zammil00@users.noreply.github.com>
2024-07-14 20:31:01 +0700

    Update img

---
+++ src/assets/img/index.js
@@ -0,0 +1,9 @@
+import rumah1 from "./rumah1.jpg";
+import rumah2 from "./rumah2.jpg";
+import rumah3 from "./rumah3.jpg";
+import rumah4 from "./rumah4.jpg";
+import rumah5 from "./rumah5.jpg";
+import rumah6 from "./rumah6.jpg";
+import rumah7 from "./rumah7.jpg";
+
+export { rumah1, rumah2, rumah3, rumah4, rumah5, rumah6, rumah7 };
